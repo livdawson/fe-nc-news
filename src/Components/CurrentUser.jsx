f73f9b6e9a5069ee8944dda5b53d42c14f1debdc
livdawson <olivia.dawson@btinternet.com>
2024-02-13 21:53:18 +0000

    improve mobile view of navigation bar

--- src/Components/CurrentUser.jsx
+++ src/Components/CurrentUser.jsx
@@ -1,21 +1,47 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import ChangeUser from "./ChangeUser";
 import UserContext from "./UserContext";
+import { Avatar } from "@mui/material";
 
 export default function CurrentUser({className}) {
     const { loggedInUser } = useContext(UserContext);
     const [openDialog, setOpenDialog] = useState(false);
+    const [smallScreen, setSmallScreen] = useState(false);
+   
+    useEffect(() => {
+      const handleResize = () => {
+        setSmallScreen(window.innerWidth <= 600);
+      };
 
+      handleResize();
+      window.addEventListener("resize", handleResize);
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, []); 
+  
   return (
-    <div className={className}>
-      <p>Current user: {loggedInUser.username}</p>
-      <img
-        id="current-user-avatar"
+    <div>
+      { !smallScreen ?
+      <div className={className}> 
+        <Avatar
+        className="current-user-avatar"
         src={loggedInUser.avatar_url}
         alt={`avatar for user {loggedInUser.username}`}
-      />
+        /> 
+      <p>{loggedInUser.username}</p>
       <button onClick={() => setOpenDialog(true)}>Change User</button>
       <ChangeUser open={openDialog} onClose={() => setOpenDialog(false)} />
+      </div> : 
+      <div className={className}>
+      <Avatar
+      className="current-user-avatar"
+      src={loggedInUser.avatar_url}
+      alt={`avatar for user {loggedInUser.username}`}
+      onClick={() => setOpenDialog(true)}
+      />
+      <ChangeUser open={openDialog} onClose={() => setOpenDialog(false)} />
+      </div>}
     </div>
   );
 }
